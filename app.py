cf520cbc34ef54b5c72b307cc5a044fdf82c6e9f
Utkarsh Gupta <utk.gupta@gmail.com>
2024-05-26 10:25:56 +0530

    Updating the UI for chatbot

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ CSS = """
     flex-direction: column;
 }
 div#chatbot-column {
-    height: 70vh !important;  # Set to desired height (e.g., 650px)
+    height: 60vh !important;  # Set to desired height (e.g., 650px)
 }
 .tabitem {
     height: 80vh !important;  # Set to desired height (e.g., 650px)
@@ -37,29 +37,23 @@ with gr.Blocks(title="Inputs") as inputs:
                 in_dsg = gr.Textbox(label="Designation", container=False, placeholder="Enter designation")
                 in_dpt = gr.Textbox(label="Department", container=False, placeholder="Enter department")
     with gr.Row():
-        gr.Markdown('### Prompts')
-        with gr.Column(scale=2):
-            gr.Markdown('#### Prompt for Questions')
-            question_prompt_txt = gr.Textbox("Question Prompt", container=False, lines=3, placeholder="Question prompt")
-        with gr.Column(scale=2):
-            gr.Markdown('#### Prompt for Skills')
-            skills_prompt_txt = gr.Textbox("Skills Prompt", container=False, lines=3, placeholder="Skills prompt")
-    with gr.Row():
         gr.Markdown('### Results for Bot')
         with gr.Column(scale=2):
             gr.Markdown('#### Actual Questions')
-            question_text = gr.Textbox("Questions for Bot", container=False, lines=3, max_lines=10, placeholder="Question text", interactive=False, show_copy_button=True)
             btn1 = gr.Button("Fetch Questions for Bot")
+            question_text = gr.Textbox("Questions for Bot", container=False, lines=3, max_lines=10,
+                                       placeholder="Question text", interactive=False, show_copy_button=True)
             btn1.click(fn=interview_assistant.generate_questions,
                        inputs=[in_cmp, in_dsg, in_dpt],
-                       outputs=[question_prompt_txt, question_text])
+                       outputs=[question_text])
         with gr.Column(scale=2):
             gr.Markdown('#### Actual Skills')
-            skills_text = gr.Textbox("Skills for Bot", container=False, lines=3, max_lines=10, placeholder="Skills text", interactive=False, show_copy_button=True)
             btn2 = gr.Button("Fetch Skills for Bot")
+            skills_text = gr.Textbox("Skills for Bot", container=False, lines=3, max_lines=10,
+                                     placeholder="Skills text", interactive=False, show_copy_button=True)
             btn2.click(fn=interview_assistant.generate_skills,
                        inputs=[in_cmp, in_dsg, in_dpt],
-                       outputs=[skills_prompt_txt, skills_text])
+                       outputs=[skills_text])
 
 with gr.Blocks(css=CSS) as chat:
     gr.Markdown('## Chat with the employee')
@@ -69,7 +63,7 @@ with gr.Blocks(css=CSS) as chat:
 with gr.Blocks(title="Analyse the Skills") as analysis:
     gr.Markdown('## Analyse the Conversation for Skills')
     btn5 = gr.Button("Analyse Conversation")
-    analysis_text = gr.Textbox(label="Conversation Analysis", container=False, lines = 10, max_lines=10)
+    analysis_text = gr.Textbox(label="Conversation Analysis", container=False, lines = 10, max_lines=20)
     btn5.click(fn=interview_assistant.analyse_conversation, inputs=None, outputs=analysis_text)
 
 demo = gr.TabbedInterface(
